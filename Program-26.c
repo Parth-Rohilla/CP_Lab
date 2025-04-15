8bd06d2208f0db9ce8da5c2606180061baf75194
Parth-Rohilla <parthrohilla17@gmail.com>
2025-04-15 23:02:01 +0530

    completed program

--- Program-26.c
+++ Program-26.c
@@ -3,3 +3,18 @@ Objective:Write C program to learn iterative statements like while and do while 
 Program-26
 Write a c program to print 1 to n numbers
 */
+
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    printf("All numbers from 1 to %d:\n",n);
+    i=1;
+    while(i<=n)
+    {
+        printf("%d\t",i);
+        i++;
+    }
+    return 0;
+}
